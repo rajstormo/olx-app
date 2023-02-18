2af43aeba5f995f700b6ea54f1dcf46815acb8e8
rajstormo <67282023+rajstormo@users.noreply.github.com>
2023-02-18 16:19:42 +0530

    Updated Banner.js

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -19,20 +19,18 @@ function Banner() {
             >
               {" "}
               <option value="null">ALL CATEGORIES</option>
-              <option value="Cars">Cars</option>
-              <option value="Cameras & Lenses">Cameras & Lenses</option>
               <option value="Computers & Laptops">Computers & Laptops</option>
+              <option value="Electrical Appliances">Electrical Applicances</option>
               <option value="Mobile Phones">Mobile Phones</option>
-              <option value="Motorcycles">Motorcycles</option>
+              <option value="Electronic accessories">Electronic accessories</option>
+              <option value="Sports"> Sports </option>
               <option value="Tablets">Tablets</option>
             </select>
           </div>
           <div className="otherQuickOptions">
-            <span onClick={()=>setCategory("Cars")} >Cars</span>
-            <span onClick={()=>setCategory("Cameras & Lenses")} >Cameras & Lenses</span>
             <span onClick={()=>setCategory("Computers & Laptops")} >Computers & Laptops</span>
             <span onClick={()=>setCategory("Mobile Phones")} >Mobile Phones</span>
-            <span onClick={()=>setCategory("Motorcycles")} >Motorcycles</span>
+            <span onClick={()=>setCategory("Electrical Appliances")}>Electrical Applicances</option>
             <span onClick={()=>setCategory("Tablets")} >Tablets</span>
           </div>
         </div>
